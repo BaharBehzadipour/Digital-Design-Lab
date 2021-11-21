60f3ebfe1f12b0518c5856bb5d588f71b202fd0f
BaharBehzadipour <65755248+BaharBehzadipour@users.noreply.github.com>
2021-11-22 02:12:09 +0330

    Update dec4_16.v

--- dec4_16.v
+++ dec4_16.v
@@ -1,6 +1,6 @@
 //decoder 2 to 4 
 
-module dec2_4 (W , Enable , Y);
+module dec2_4 (W  , Y, Enable);
 
     input [1:0] W;
     input Enable;
@@ -11,11 +11,11 @@ module dec2_4 (W , Enable , Y);
 
         case ({Enable, W})
 
-            3’b100: Y = 4’b1000;
-            3’b101: Y = 4’b0100;
-            3’b110: Y = 4’b0010;
-            3’b111: Y = 4’b0001;
-            default: Y = 4’b0000;
+            3'b100: Y = 4'b1000;
+            3'b101: Y = 4'b0100;
+            3'b110: Y = 4'b0010;
+            3'b111: Y = 4'b0001;
+            default: Y = 4'b0000;
 
         endcase
 
@@ -24,7 +24,7 @@ endmodule
 
 //decoder 4 to 16 
 
-module dec4_16 (W , Enable , Y);
+module dec4_16 (W  , Y, Enable);
 
     input [3:0] W;
     input Enable;
@@ -33,7 +33,7 @@ module dec4_16 (W , Enable , Y);
 
     wire [0:3] T;
 
-    dec2_4 Dec1 (W[3:2], T[0:3], Enable);
+    dec2_4 Dec1 (W[3:2],T[0:3], Enable);
     dec2_4 Dec2 (W[1:0], Y[0:3], T[0]);
     dec2_4 Dec3 (W[1:0], Y[4:7], T[1]);
     dec2_4 Dec4 (W[1:0], Y[8:11], T[2]);
