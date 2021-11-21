6365c3722f786dea8351651452f9c6e85e99aa9d
BaharBehzadipour <65755248+BaharBehzadipour@users.noreply.github.com>
2021-11-22 02:13:24 +0330

    Update mux2_64bit_tb.v

--- mux2_64bit_tb.v
+++ mux2_64bit_tb.v
@@ -6,71 +6,70 @@ module mux2_64bit_tb;
 reg w0, w1, s;
 wire f;
 
-mux2_64bit uut (w0, w1, s, f)
+mux2_64bit uut (.w0(w0), .w1(w1), .s(s) , .f(f));
 
-initial 
-begin
-$dumpfile("mux2_64bit_tb.vcd")
-$dumpvars(0,mux2_64bit_tb)
- 
-w0=0
-w1=0
-s=0
+initial begin
+$dumpfile("mux2_64bit_tb.vcd");
+$dumpvars(0,mux2_64bit_tb);
+
+w0=0;
+w1=0;
+s=0;
 if(f==1)
   $display("test fail");
 #20
 
-w0=0
-w1=1
-s=0
+w0=0;
+w1=1;
+s=0;
 if(f==1)
   $display("test fail");
 #20
 
 
-w0=1
-w1=0
-s=0
+w0=1;
+w1=0;
+s=0;
 if(f==0)
   $display("test fail");
 #20
 
 
-w0=1
-w1=1
-s=0
+w0=1;
+w1=1;
+s=0;
 if(f==0)
   $display("test fail");
 #20
 
 
-w0=0
-w1=0
-s=1
+w0=0;
+w1=0;
+s=1;
 if(f==1)
   $display("test fail");
 #20
 
 
-w0=0
-w1=1
-s=1
+w0=0;
+w1=1;
+s=1;
 if(f==0)
   $display("test fail");
 #20
 
 
-w0=1
-w1=0
-s=1
+w0=1;
+w1=0;
+s=1;
 if(f==1)
   $display("test fail");
 #20
 
 
-w0=1
-w1=1
-s=1
+w0=1;
+w1=1;
+s=1;
 if(f==0)
   $display("test fail");
 #20
